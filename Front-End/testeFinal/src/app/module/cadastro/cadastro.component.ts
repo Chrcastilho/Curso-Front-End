d33d994bfc82f8f0a40787299c31f4d283e38b9f
Christian Andrade Fernandes Castilho <chrcastilho@gmail.com>
2020-01-07 00:06:43 -0300

    Cadastro
    90% realizado falta funcionalidade dos botoes e ajuste no footer do conteiner

--- Front-End/testeFinal/src/app/module/cadastro/cadastro.component.ts
+++ Front-End/testeFinal/src/app/module/cadastro/cadastro.component.ts
@@ -9,27 +9,50 @@ import { Router } from '@angular/router';
 })
 export class CadastroComponent implements OnInit {
   private formCadastro: FormGroup;
+
   ngOnInit() {
     this.creatForm(new Cadastro());
   }
+
+  public estadoList = [
+    { id: 1, uf: 'AC' },
+    { id: 2, uf: 'AL' },
+    { id: 3, uf: 'AP' },
+    { id: 4, uf: 'AM' },
+    { id: 5, uf: 'BA' },
+    { id: 6, uf: 'CE' },
+    { id: 7, uf: 'DF' },
+    { id: 8, uf: 'ES' },
+    { id: 9, uf: 'GO' },
+    { id: 10, uf: 'MA' },
+    { id: 11, uf: 'MT' },
+    { id: 12, uf: 'MS' },
+    { id: 13, uf: 'MG' },
+    { id: 14, uf: 'PA' },
+    { id: 15, uf: 'PB' },
+    { id: 16, uf: 'PR' },
+    { id: 17, uf: 'PE' },
+    { id: 18, uf: 'PI' },
+    { id: 19, uf: 'RJ' },
+    { id: 20, uf: 'RN' },
+    { id: 21, uf: 'RS' },
+    { id: 22, uf: 'RO' },
+    { id: 23, uf: 'RR' },
+    { id: 24, uf: 'SC' },
+    { id: 25, uf: 'SP' },
+    { id: 26, uf: 'SE' },
+    { id: 27, uf: 'TO' },
+  ]
+
+  public sexoList = [
+    { id: 1, sexo: 'M' },
+    { id: 2, sexo: 'F' }
+  ]
 
   constructor(
     private fb: FormBuilder,
     private router: Router
   ) { }
-
-  validaCampos() {
-    if (this.formCadastro.valid) {
-      this.router.navigate(['cadastro']);
-    }
-    else {
-      Object.keys(this.formCadastro.controls).filter((controlName) => {
-        if (this.formCadastro.controls[controlName].invalid) {
-          alert(`${controlName} e obrigatorio`);
-        }
-      })
-    }
-  }
 
   creatForm(Cadastro: Cadastro) {
     this.formCadastro = this.fb.group({
@@ -40,7 +63,7 @@ export class CadastroComponent implements OnInit {
           Validators.maxLength(200)
         ])],
       sobreNome: [
-        'Teste Nome',
+        'Teste Sobre Nome',
         Validators.compose([
           Validators.required,
           Validators.maxLength(200)
@@ -62,7 +85,7 @@ export class CadastroComponent implements OnInit {
           Validators.required,
           Validators.maxLength(100)
         ])],
-        estadoCivil: [
+      estadoCivil: [
         'Casado',
         Validators.compose([
           Validators.required,
@@ -87,12 +110,12 @@ export class CadastroComponent implements OnInit {
           Validators.maxLength(500)
         ])],
       uf: [
-        1,
+        16,
         Validators.compose([
           Validators.required
         ])],
       cidade: [
-        '1',
+        'Maringa',
         Validators.compose([
           Validators.required,
           Validators.maxLength(200)
@@ -111,33 +134,47 @@ export class CadastroComponent implements OnInit {
       endereco: [''],
       numero: [''],
       complemento: [''],
-      uf: [1],
+      uf: [16],
       cidade: [''],
     })
   }
 
-  cancelForm() {
+  cancelButton() {
     this.resetForm;
     this.router.navigate(['cadastro']);
   }
 
+  saveButton() {
+    if (this.formCadastro.valid) {
+      this.router.navigate(['cadastro']);
+      this.allUsersData();
+    }
+    else {
+      Object.keys(this.formCadastro.controls).filter((controlName) => {
+        if (this.formCadastro.controls[controlName].invalid) {
+          alert(`${controlName} e obrigatorio`);
+        }
+      })
+    }
+  }
+
   onSubmit() {
-    console.log(this.formCadastro.value);
   }
 
-  get email() {
-    return this.formCadastro.get('email');
-  }
-
-  get senha() {
-    return this.formCadastro.get('senha');
-  }
-
-  goToCadastro() {
-    this.router.navigate(['cadastro']);
+  private allUsersData() {
+    const nome = this.formCadastro.get('nome').value;
+    const sobreNome = this.formCadastro.get('sobreNome').value;
+    const idade = this.formCadastro.get('idade').value;
+    const sexo = this.formCadastro.get('sexo').value;
+    const estado = this.formCadastro.get('estado').value;
+    const estadoCivil = this.formCadastro.get('estadoCivil').value;
+    const endereco = this.formCadastro.get('endereco').value;
+    const numero = this.formCadastro.get('numero').value;
+    const complemento = this.formCadastro.get('complemento').value;
+    const uf = this.formCadastro.get('uf').value;
+    const cidade = this.formCadastro.get('cidade').value;
   }
 }
-
 export class Cadastro {
   email: string = '';
   senha: string = '';
